51eb8bd1fcb21b7b3b3dd0e4fb24cd194390a265
TanuiDev <bk.kibiwott@gmail.com>
2026-07-22 14:37:33 +0300

    Updated

--- src/saved/saved.controller.ts
+++ src/saved/saved.controller.ts
@@ -1,4 +1,27 @@
 import { Controller } from '@nestjs/common';
+import { SavedService } from './saved.service';
+import { Saved } from './entities/saved.entity';
 
 @Controller('saved')
-export class SavedController {}
+export class SavedController {
+  constructor(private savedService: SavedService) {}
+  async getAllSaved(): Promise<Saved[]> {
+    return this.savedService.getAllSaved();
+  }
+
+  async getSavedById(id: number): Promise<Saved> {
+    return this.savedService.getSavedById(id);
+  }
+
+  async createSaved(saved: Saved): Promise<Saved> {
+    return this.savedService.createSaved(saved);
+  }
+
+  async getSavedByUserId(userId: number): Promise<Saved[]> {
+    return this.savedService.getSavedByUserId(userId);
+  }
+
+  async deleteSaved(id: number): Promise<{ message: string }> {
+    return this.savedService.deleteSaved(id);
+  }
+}
--- src/saved/saved.service.ts
+++ src/saved/saved.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Saved } from './entities/saved.entity';
 
 @Injectable()
-export class SavedService {}
+export class SavedService {
+  constructor(
+    @InjectRepository(Saved)
+    private savedRepository: Repository<Saved>,
+  ) {}
+
+
+  async getAllSaved(): Promise<Saved[]> {
+    return this.savedRepository.find();
+  }
+
+  async getSavedById(id: number): Promise<Saved> {
+    return this.savedRepository.findOne({ where: { id } });
+  }
+  async createSaved(saved: Saved): Promise<Saved> {
+    return this.savedRepository.save(saved);
+  }
+
+  async deleteSaved(id: number): Promise<{ message: string }> {
+    const saved = await this.savedRepository.findOne({ where: { id } });
+    if (!saved) {
+      throw new NotFoundException('Saved item not found');
+    }
+    await this.savedRepository.remove(saved);
+    return { message: 'Saved item deleted successfully' };
+  }
+}
