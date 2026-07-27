560d2b5f9d2b759ad1ff55340134c5f44ebbcee7
TanuiDev <bk.kibiwott@gmail.com>
2026-07-27 20:05:10 +0300

    Updated

--- src/saved/saved.module.ts
+++ src/saved/saved.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SavedService } from './saved.service';
 import { SavedController } from './saved.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Saved } from './entities/saved.entity';
 @Module({
   providers: [SavedService],
   controllers: [SavedController],
+  imports: [TypeOrmModule.forFeature([Saved])],
 })
 export class SavedModule {}
